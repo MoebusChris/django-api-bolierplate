79542540bb00fb5588ce60580a12cceb15f38c76
Subeom Jeong <cgray@Subeoms-Mini.attlocal.net>
2024-05-08 22:45:58 -0700

    12 - Configure setting and finish off

--- cores/project/settings/base.py
+++ cores/project/settings/base.py
@@ -1,6 +1,6 @@
 from typing import List
 
-DEBUG = False
+DEBUG = True
 SECRET_KEY = NotImplemented
 
 ALLOWED_HOSTS: List[str] = ['*']
@@ -101,8 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
+AUTH_USER_MODEL = 'users.User'
 
 LANGUAGE_CODE = 'en-us'
 
@@ -112,9 +111,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'  # type: ignore # noqa: F821
 
@@ -123,8 +119,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'  # type: ignore # noqa: F821
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10MB
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
